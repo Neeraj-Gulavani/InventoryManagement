af6c0519b0c5c2486653c5dfa3c09f3fd4ea5230
Neeraj-Gulavani <neerajgulavani@gmail.com>
2025-02-13 19:12:08 +0530

    Created Login Authentication System

--- InventoryManagementSystem/content/views.py
+++ InventoryManagementSystem/content/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login:index'))
     return render(request,'content/index.html')
--- InventoryManagementSystem/login/urls.py
+++ InventoryManagementSystem/login/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+app_name = 'login'
 urlpatterns = [
     path('',views.index,name="index")
+    #path('login',views.login_request,name="loginpage")
 ]
