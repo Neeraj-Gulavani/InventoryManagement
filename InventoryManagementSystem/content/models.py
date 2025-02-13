44f09a03bc15617956fd0f4bc51ffa495af939e8
Neeraj-Gulavani <neerajgulavani@gmail.com>
2025-02-13 19:40:14 +0530

    Setup Database for Inventory Assets

--- InventoryManagementSystem/content/models.py
+++ InventoryManagementSystem/content/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Asset(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64) #product name
     count = models.IntegerField() #quantity in stock
     cost=models.FloatField() #cost price
     price=models.FloatField() #sale price
--- InventoryManagementSystem/login/urls.py
+++ InventoryManagementSystem/login/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 app_name = 'login'
 urlpatterns = [
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('logout',views.logout_view,name="logout")
     #path('login',views.login_request,name="loginpage")
 ]
