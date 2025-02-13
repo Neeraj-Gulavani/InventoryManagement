2b38bed1aa03dad09f53152d6d6da6bd161401f4
Neeraj-Gulavani <neerajgulavani@gmail.com>
2025-02-13 19:12:08 +0530

    Created Login Authentication System

--- InventoryManagementSystem/content/models.py
+++ InventoryManagementSystem/content/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Asset(models.Model):
+    name = models.CharField(max_length=64)
+    count = models.IntegerField() #quantity in stock
+    cost=models.FloatField() #cost price
+    price=models.FloatField() #sale price
--- InventoryManagementSystem/login/views.py
+++ InventoryManagementSystem/login/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate,login,logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 # Create your views here.
 def index(request):
-    return render(request,'login/index.html')
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return HttpResponseRedirect(reverse('login:index'))
+        else:
+            return render(request,'login/index.html',{
+                "message": "Invalid Credentials"
+            })
+    if not request.user.is_authenticated:
+        return render(request,'login/index.html')
+    else:
+        return HttpResponseRedirect(reverse('content:index'))
+    
+def logout_view(request):
+    logout(request)
+    return render(request,"login/index.html",{
+        "message": "Logged Out."
+    })
